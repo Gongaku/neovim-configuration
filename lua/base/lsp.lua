bdcb13f419fed0f5b2f1e7fbaa94dd5393c6d952
Gongaku <132847661+Gongaku@users.noreply.github.com>
2025-11-05 22:31:36 -0700

    Updated LSP config to only be active on non-NixOS systems

--- lua/base/lsp.lua
+++ lua/base/lsp.lua
@@ -6,8 +6,16 @@ local lsp_list = {
   "tinymist"   -- Typst
 }
 
-require('mason').setup()
-require('mason-lspconfig').setup({ ensure_installed = lsp_list })
+local fd_os_release = assert(io.open("/etc/os-release"), "r")
+local s_os_release = fd_os_release:read("*a")
+fd_os_release:close()
+s_os_release = s_os_release:lower()
+local is_nixos = s_os_release:match("nixos")
+
+if is_nixos == nil then
+	require('mason').setup()
+	require('mason-lspconfig').setup({ ensure_installed = lsp_list })
+end
 
 vim.lsp.enable(lsp_list)
 
