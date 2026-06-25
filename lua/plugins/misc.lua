a48b7be36a9317866e25781ebc68e79043b30608
Gongaku <132847661+Gongaku@users.noreply.github.com>
2026-06-24 20:40:28 -0600

    WIP: Disabled dependencies_bin param for typst-preview

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,10 +2,10 @@ require("ibl").setup()
 require("focus").setup()
 
 if not helpers.is_work then
-  require("typst-preview").setup({
-    dependencies_bin = {
-      tinymist = 'tinymist',
-      websocat = 'websocat'
-    },
-  })
+  -- require("typst-preview").setup({
+  --   dependencies_bin = {
+  --     tinymist = 'tinymist',
+  --     websocat = 'websocat'
+  --   },
+  -- })
 end
