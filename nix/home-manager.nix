192433d4b2041e7bd78e6ef6c8e263e24ff2f7ab
Gongaku <132847661+Gongaku@users.noreply.github.com>
2026-06-29 22:46:44 -0600

    feat(hm): install neovim plugins from lock file via nix

--- nix/home-manager.nix
+++ nix/home-manager.nix
@@ -14,36 +14,39 @@ inputs:
   lib,
   ...
 }:
+let
+  plugins = import ./plugins.nix { inherit pkgs lib; };
+in
 {
   programs.neovim = {
     enable = true;
     package = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.default;
     withPython3 = true;
     withNodeJs = true;
+    plugins = map (p: { plugin = p; }) plugins;
     extraPackages = with pkgs; [
       # keep-sorted start
-      bash-language-server # Bash language with `shellcheck` & `shfmt` support
-      harper # Grammar Checker for developers
-      inotify-tools # Simple interface to `inotify` for filesystem events
-      lua # Lua Language
-      lua-language-server # Lua Language Server
-      nixd # Nix language server with rich diagnostics
-      nixfmt # Nix Formatter
-      pyright # Python language server
-      ruff # Python linter
-      shellcheck # Shell Formatter/Validator
-      shfmt # Shell Formatter
-      tinymist # LSP server for Typst
-      tree-sitter # Syntax highlighting
-      typst # Typst Language
-      websocat # Web Sockets
-      yaml-language-server # YAML LSP
+      bash-language-server
+      harper
+      inotify-tools
+      lua
+      lua-language-server
+      nixd
+      nixfmt
+      pyright
+      ruff
+      shellcheck
+      shfmt
+      tinymist
+      tree-sitter
+      typst
+      websocat
+      yaml-language-server
       # keep-sorted end
     ];
   };
 
   xdg = {
-    # `$XDG_CONFIG_HOME` (`$HOME/.config`)
     configFile = {
       "nvim/init.lua".source = ../init.lua;
       "nvim/lua".source = ../lua;
@@ -54,7 +57,6 @@ inputs:
         ignore = E226,E302,E401,W503,E501
       '';
     };
-    # `$XDG_STATE_HOME` (`$HOME/.local/state`)
     stateFile = {
       "nvim/mason/packages/lua-language-server/libexec/bin/lua-language-server".source =
         "${pkgs.lua-language-server}/bin/lua-language-server";
